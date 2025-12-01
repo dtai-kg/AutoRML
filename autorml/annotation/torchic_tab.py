d6d5a8fc3988034e7495816e0850df45053221cb
Fotis Kalioras <fkalioras@gmail.com>
2025-12-01 16:51:29 +0100

    fix: refactor packages paths, pep8 style fixes

--- autorml/annotation/torchic_tab.py
+++ autorml/annotation/torchic_tab.py
@@ -1,11 +1,12 @@
 from torchic_tab_heuristic.task_calculation_wd import generate_annotations #Closed source. Contact for access
 
+
 #Call of TorchicTab to extract tabular annotations for a given table
 def torchic_tab(file):
 
     es_index = False
-    (subject_column, primary_annotations, secondary_annotations, 
-    cea, cpa, cta) = generate_annotations(file, es_index)
+    (subject_column, primary_annotations, secondary_annotations,
+     cea, cpa, cta) = generate_annotations(file, es_index)
     cqa = []
 
     return (subject_column, primary_annotations, secondary_annotations, 
